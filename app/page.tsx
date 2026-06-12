b8b57558b83f583097cbb6d9fc83422156795d8a
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-06-12 16:46:15 +0530

    Removed all the unwanted imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,19 @@
 import React from 'react'
 import HeroSection from './_components/Home/HeroSection'
-import ServiceSection from './_components/ServiceSection'
-import AboutSection from './_components/AboutSection'
 import SkillSection from './_components/SkillSection'
-import ProjectSection from './_components/ProjectSection'
-import EducationSection from './_components/EducationSection'
 import { TestimonialsSection } from './_components/TestimonialSection'
-import { PricingSection } from './_components/WorkingStyle'
 import ContactSection from './_components/ContactSection'
-import CtaSection from './_components/CtaSection'
-import Navbar from './_components/Navbar/Navbar'
 import ExperienceView from './_components/Experience/experience-view'
 import ProjectView from './_components/Projects/project-view'
 
 const page = () => {
   return (
     <div className="max-w-4xl mx-auto px-4">
-      {/* // Background pattern with radial gradient and opacity */}
-
       <HeroSection />
       <SkillSection />
       <ExperienceView />
       <ProjectView />
       <TestimonialsSection />
-      {/* <ServiceSection />
-      <AboutSection />
-      <EducationSection /> */}
-
-      {/* <ProjectSection />
-
-      <PricingSection />
-      <CtaSection /> */}
       <ContactSection />
 
     </div>
