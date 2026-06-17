4ba761430c98ab7704701447095d77aaee41c0ee
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-06-17 20:37:49 +0530

    AnimatedContent added for hero section

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,26 @@ import { TestimonialsSection } from './_components/TestimonialSection'
 import ContactSection from './_components/ContactSection'
 import ExperienceView from './_components/Experience/experience-view'
 import ProjectView from './_components/Projects/project-view'
+import AnimatedContent from './_components/AnimateVerticleContent'
 
 const page = () => {
   return (
     <div className="max-w-4xl mx-auto px-4">
-      <HeroSection />
+      <AnimatedContent
+        distance={100}
+        direction="vertical"
+        reverse={false}
+        duration={0.8}
+        ease="power3.out"
+        initialOpacity={0}
+        animateOpacity
+        scale={1}
+        threshold={0.1}
+        delay={0.1}
+      >
+        <HeroSection />
+      </AnimatedContent>
+
       <SkillSection />
       <ExperienceView />
       <ProjectView />
